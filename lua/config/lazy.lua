71a08e0736735b464d8502b63f7f08bdc9d110c0
Joni Eronen <joni.eronen@avanio.com>
2025-07-22 13:21:10 +0300

    Set concurrency

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  concurrency = 10,
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
